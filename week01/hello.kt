e8ae63bc0cbe980cb7446d3e4e61c5d25a15081b
itse1329spring23 <123481003+itse1329spring23@users.noreply.github.com>
2023-01-24 09:47:29 -0600

    Create hello.kt
    This is my first Kotlin program.

---
+++ week01/hello.kt
@@ -0,0 +1,9 @@
+/**
+ * My first Kotlin file / Francisco Rodriguez
+ */
+
+fun main() {
+  println("Hello World!")
+  println("I am learning Kotlin.")
+  println("Rainy days and Mondays always get me down.")
+}
